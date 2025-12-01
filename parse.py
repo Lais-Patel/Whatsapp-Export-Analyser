00159d5c4a7b80ec73abb8493a94e7e2e71af593
Mango <plais999@gmail.com>
2025-12-01 15:18:57 +0000

    Improved Parsing
    parsing the data now works, and improved the code layout to be more modular and readable overall. Further improvements would include cleaning logic, checking the data range, adding support for media

--- parse.py
+++ parse.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import pandas as pd
 
 # Whatsapp Export file name
-file_name = ""
+file_name = "Whatsapp-Export-Analyser\data\whatsapp_data"
 
 exclude_people = False # Set to False to exclude names in excluded from dataset
 exclude_metaAI = False # Set to False to exclude Meta AI from dataset
@@ -14,32 +14,57 @@ before_date = "15/12/2026" # Choose when to stop collecting data from dd/mm/yyyy
 chat_log = []
 check_data = False
 
-with open(file_name, encoding="utf8") as f:
+
+def add_log(timestamp, msg, chat_log):
+    data = [datetime.strptime(timestamp[1:], "%d/%m/%Y, %H:%M - ")]
+    data.append(msg[0])
+    data.append(msg[1][1:])
+    chat_log.append(data)
+
+def check_excldues(name):
+    with open("Whatsapp-Export-Analyser\excluded.txt") as ef:
+        add_to_log = True
+
+        for exclude_name in ef.read().split():
+            if exclude_name == name:
+                add_to_log = False
+        
+        if name == "Meta AI" and exclude_metaAI:
+            add_to_log = False
+
+    return add_to_log
+
+def check_data_range(timestamp, check_data):
+    if check_data == False and (timestamp[1:11] == after_date):
+        check_data = True
+    elif check_data == True and (timestamp[1:11] == before_date):
+        check_data = False
+    return check_data
+
+def save_to_csv(chat_log):
+    df = pd.DataFrame(chat_log, columns=["Datetime","Name","Message"])
+    df = df.set_index("Datetime")
+    df = df.sort_values("Datetime")
+    df.to_csv(file_name+".csv")
+
+
+with open(file_name+".txt", encoding="utf8") as f:
     log = f.read()
     timestamps = re.findall(r"\n\d{2}/\d{2}/\d{4}, \d{2}:\d{2} - ", log)
     messages = re.split(r"\n\d{2}/\d{2}/\d{4}, \d{2}:\d{2} - ", log)[1:]
+
     for i,timestamp in enumerate(timestamps):
         if messages[i].find(":",3,15)>-1:
             msg = messages[i].replace("\n"," ")
             msg = msg.split(":",1)
-            if check_data == False and (timestamp[1:11] == after_date):
-                check_data = True
-                print("true")
-            elif check_data == True and (timestamp[1:11] == before_date):
-                check_data = False
-                print("false")
+            check_data = check_data_range(timestamp,check_data)
             if (check_data == True):
-                if (msg[0] == "Meta AI" and exclude_metaAI == False) or msg[0] != "Meta AI": 
+                if exclude_people:
+                    if check_excldues(msg[0]):
+                        if msg[1][:6] != " POLL:":
+                            add_log(timestamp, msg, chat_log)
+                else:
                     if msg[1][:6] != " POLL:":
-                        data = [datetime.strptime(timestamp[1:], "%d/%m/%Y, %H:%M - ")]
-                        data.append(msg[0])
-                        data.append(msg[1][1:])
-                        chat_log.append(data)
+                            add_log(timestamp, msg, chat_log)
 
-df = pd.DataFrame(chat_log, columns=["Datetime","Name","Message"])
-df = df.set_index("Datetime")
-df = df.sort_values("Datetime")
-df["Message_Count"] = 1
-df["Cum_Messages_Overall"] = df["Message_Count"].cumsum()
-df["Cum_Messages"] = df.groupby("Name")["Message_Count"].cumsum()
-df.to_csv("data.csv")
+save_to_csv(chat_log)
